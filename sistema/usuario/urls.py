1e2bf8143be46bf82f5ee5c7e9c19f0e4228ec66
MarcosFrMelo <143053350+MarcosFrMelo@users.noreply.github.com>
2025-11-21 10:06:50 -0300

    feat: atualiza endpoint de registro de usuário para 'registrar-api'

--- sistema/usuario/urls.py
+++ sistema/usuario/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import RegisterAPI
 
 urlpatterns = [
-    path('registrar/', RegisterAPI.as_view(), name='api_registrar'),
+    path('registrar-api/', RegisterAPI.as_view(), name='api_registrar'),
 ]
