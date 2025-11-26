60c802076dee3d34620f109e7d58e9810f04c5fc
MarcosFrMelo <143053350+MarcosFrMelo@users.noreply.github.com>
2025-11-26 17:40:22 -0300

    Refatorar a estrutura do código para melhorar a legibilidade e a manutenção

--- sistema/usuario/serializers.py
+++ sistema/usuario/serializers.py
@@ -2,15 +2,21 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
         fields = ('id', 'username', 'password1', 'password2')
-        extra_kwargs = {'password1': {'write_only': True}, 'password2': {'write_only': True}}
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError({'password1': "As senhas não conferem."})
+        return data
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            password1=validated_data['password1'],
-            password2=validated_data['password2']
+            password=validated_data['password1'] 
         )
         return user
